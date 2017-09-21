732f2a05e3f002eccb228a2031b00b6a2a628567
Andrei Herdt <Andrei.Herdt@bmw.de>
2017-09-21 11:18:19 +0200

    Add keras
    Change-Id: I86c0639f36772802d985f14d8ed5db268919e7ed

--- hw1/miscellaneous.py
+++ hw1/miscellaneous.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import gym
-
+#from keras.models import Sequential
+#from keras.layers import Dense, Lambda
 
 # Obtain data through simulation with OpenAI
 def rollout(env, policy_fun, render):
@@ -15,7 +16,7 @@ def rollout(env, policy_fun, render):
     done = False
     while not done:
         action = policy_fun(obs[None,:])
-        observations.append(obs.reshape(1,11))
+        observations.append(obs)
 
         actions.append(action)
         obs, r, done, _ = env.step(action)
@@ -29,10 +30,10 @@ def rollout(env, policy_fun, render):
     return observations, actions
 
 
-def neural_network(input):
+def neural_network(input, dimensions):
 
     with tf.name_scope('layer1'):
-        W0 = tf.Variable(tf.truncated_normal([11, 30], stddev=0.1))
+        W0 = tf.Variable(tf.truncated_normal([dimensions[0], 30], stddev=0.1))
         b0 = tf.Variable(tf.constant(value=0.1, shape=[30]))
         h0 = tf.matmul(input, W0) + b0
 
@@ -42,8 +43,17 @@ def neural_network(input):
         h1 = tf.matmul(h0, W1) + b1
 
     with tf.name_scope('layer3'):
-        W2 = tf.Variable(tf.truncated_normal([30, 3], stddev=0.1))
-        b2 = tf.Variable(tf.constant(value=0.1, shape=[3]))
+        W2 = tf.Variable(tf.truncated_normal([30, dimensions[1]], stddev=0.1))
+        b2 = tf.Variable(tf.constant(value=0.1, shape=[dimensions[1]]))
         h2 = tf.matmul(h1, W2) + b2
 
     return h2
+
+
+#ef baseline_model():
+#   model = Sequential()
+#   model.add(Dense(input_dim/2, input_dim=input_dim, init='normal', activation='relu'))
+#   model.add(Dense(output_dim, init='normal'))
+
+#   model.compile(loss='mse', optimizer='adam', metrics=['mae'])
+#   return model
